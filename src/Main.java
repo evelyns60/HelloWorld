331deceef0d904e15c919f67f491905dbcd6230a
evellyns60 <evelyns60@nycstudents.net>
2022-09-22 10:13:45 -0400

    second test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args)
     {
+        String object = "a drink?";
         System.out.println("Hello world! My name is Evelyn Sionov");
+        System.out.println("Would you like " + object);
+
     }
 }
